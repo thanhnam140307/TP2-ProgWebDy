b10655b20865ab7e654e836dd0706cbd70a428c6
Charly Paradis <charlyparadis@hotmail.com>
2025-04-15 15:51:02 -0400

    Fin prog 4/15/25

--- cart.php
+++ cart.php
@@ -26,9 +26,53 @@
         <?php endif; ?>
     </nav>
 
-    <body>
+    <main>
+        <?php if (true): ?>
+            <section class="cart-empty">
+                <h2>Votre panier est vide.</h2>
+                <p>Pour le remplir, consultez notre liste de produits.</p>
+            </section>
+        <?php endif;
+        if (false): ?>
+            <section class="product-list">
+                <div class="product">
+                    <img class="image" src="img/AW453271.png" alt="Sac à dos">
 
-    </body>
+                    <div class="name">Sac à dos</div>
+                    <div class="price">65.99 $</div>
+                    <div class="quantity">Quantité : 1</div>
+
+                    <button class="hyperlink">Supprimer</button>
+                </div>
+
+                <div class="product">
+                    <img class="image" src="img/AW632147.png" alt="Tuque rouge">
+
+                    <div class="name">Tuque rouge</div>
+                    <div class="price">5.99 $</div>
+                    <div class="quantity">Quantité : 2</div>
+
+                    <button class="hyperlink">Supprimer</button>
+                </div>
+
+                <div class="product">
+                    <img class="image" src="img/CW154789.png" alt="Manteau d'hiver Ursidae">
+
+                    <div class="name">Manteau d'hiver Ursidae</div>
+                    <div class="price">209.99 $</div>
+                    <div class="quantity">Quantité : 1</div>
+
+                    <button class="hyperlink">Supprimer</button>
+                </div>
+            </section>
+            <section class="cart-total">
+                Total : <strong>287.96 $</strong>
+            </section>
+            <form class="cart-checkout" method="post">
+                <button>Passer la commande</button>
+            </form>
+        <?php endif; ?>
+    </main>
     <footer class="footer">
         Copyright @2025 La Baie Ourson Inc.
     </footer>
