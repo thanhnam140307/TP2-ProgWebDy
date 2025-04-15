5ca7e7173816884cf4370e6c656a3651e760cd08
Charly Paradis <charlyparadis@hotmail.com>
2025-04-15 15:10:36 -0400

    Modification database.php

--- src/database.php
+++ src/database.php
@@ -2,7 +2,7 @@
 
 function connect_db() : PDO {
     // TODO : Il vous faudra modifier le nom de la base de données et les identifiants.
-    return new PDO("mysql:host=localhost;dbname=example", "username", "password", [
+    return new PDO("mysql:host=localhost;dbname=tp2", "root", "", [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_EMULATE_PREPARES => false
