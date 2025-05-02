77d33f139f08e902411793a82efd4d51a147102c
Thanh Nam Nguyen <thanhnam140307@gmail.com>
2025-05-02 11:51:25 -0400

    Ajouter test pour CreateUserDTO.class.php (à être travaillé)

---
+++ tests/CreateUserDTOTest.php
@@ -0,0 +1,111 @@
+<?php
+//Par Thanh Nam Nguyen
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . "\CreateUserDTO.class.php"; //C:\xampp\htdocs\PHPUnit\ZipCode.php
+
+class CreateUserDTOTest extends TestCase
+{
+    private const INPUT_TYPE_EMAIL = "email";
+    private const INPUT_TYPE_PASSWORD = "password";
+
+    public function testCanCreateUserDTO()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars("newEmail@gmail.com"), htmlspecialchars("12345678"), htmlspecialchars("12345678"));
+
+        //Act
+        $email = $userDTO->getEmail();
+        $password = $userDTO->getPassword();
+        $confirmPassword = $userDTO->getConfirmPassword();
+
+        // Assert
+        $this->assertEquals("newEmail@gmail.com", $email);
+        $this->assertEquals("12345678", $password);
+        $this->assertEquals("12345678", $confirmPassword);
+    }
+
+    public function testMethodIsEmptyCanCheckIfInputsEmpty()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars(""), htmlspecialchars(""), htmlspecialchars(""));
+
+        //Act
+        $email = $userDTO->getEmail();
+        $password = $userDTO->getPassword();
+        $confirmPassword = $userDTO->getConfirmPassword();
+
+        // Assert
+		$this->assertTrue($userDTO->isEmpty($email));
+        $this->assertTrue($userDTO->isEmpty($password));
+		$this->assertTrue($userDTO->isEmpty($confirmPassword));
+    }
+
+    public function testMethodIsEqualToRegexReturnTrueIfEmailAndPasswordFilledAndRespectRegex()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars("newEmail@gmail.com"), htmlspecialchars("12345678"), htmlspecialchars("12345678"));
+
+        //Act
+        $isEmailValide = $userDTO->isEqualToRegex(self::INPUT_TYPE_EMAIL);
+        $isPasswordValide = $userDTO->isEqualToRegex(self::INPUT_TYPE_PASSWORD);
+
+        // Assert
+		$this->assertTrue($isEmailValide);
+        $this->assertTrue($isPasswordValide);
+    }
+
+    public function testMethodIsEqualToRegexReturnFalseIfEmailAndPasswordFilledAndNotRespectRegex()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars("newEmail@gmail."), htmlspecialchars("1234567"), htmlspecialchars("1234567"));
+
+        //Act
+        $isEmailValide = $userDTO->isEqualToRegex(self::INPUT_TYPE_EMAIL);
+        $isPasswordValide = $userDTO->isEqualToRegex(self::INPUT_TYPE_PASSWORD);
+
+        // Assert
+		$this->assertFalse($isEmailValide);
+        $this->assertFalse($isPasswordValide);
+    }
+
+    public function testMethodIsEqualToRegexReturnTrueIfEmailAndPasswordEmpty()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars(""), htmlspecialchars(""), htmlspecialchars(""));
+
+        //Act
+        $isEmailValide = $userDTO->isEqualToRegex(self::INPUT_TYPE_EMAIL);
+        $isPasswordValide = $userDTO->isEqualToRegex(self::INPUT_TYPE_PASSWORD);
+
+        // Assert
+		$this->assertTrue($isEmailValide);
+        $this->assertTrue($isPasswordValide);
+    }
+
+    public function testMethodIsConfirmPasswordCorrectReturnTrueIfConfirmPasswordEqualPassword()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars("newEmail@gmail.com"), htmlspecialchars("12345678"), htmlspecialchars("12345678"));
+
+        //Act
+        $isConfirmPasswordCorrect = $userDTO->isConfirmPasswordCorrect();
+
+        // Assert
+		$this->assertTrue($isConfirmPasswordCorrect);
+    }
+
+    public function testMethodIsConfirmPasswordCorrectReturnFalseIfConfirmPasswordNotEqualPassword()
+    {
+        // Arrange
+        $userDTO = new UserDTO(htmlspecialchars("newEmail@gmail.com"), htmlspecialchars("12345678"), htmlspecialchars("1234567"));
+
+        //Act
+        $isConfirmPasswordCorrect = $userDTO->isConfirmPasswordCorrect();
+
+        // Assert
+		$this->assertFalse($isConfirmPasswordCorrect);
+    }
+    //vendor\bin\phpunit tests
+}
